f32a69d3fd616c9af5c804cccd1631d636238c99
DevAggarwal23 <aggarwaldeva789@gmail.com>
2025-08-12 11:16:04 +0530

    Added Subtraction.java

--- Subtraction.java
+++ Subtraction.java
@@ -1,6 +1,6 @@
 public class Subtraction {
     public static void main(String[] args) {
-        int a = 5, b = 3;
+        int a = 10, b = 4;
         System.out.println("Difference: " + (a - b));
     }
 }
